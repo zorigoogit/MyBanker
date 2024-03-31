a9073b59ec888eb2399f826dffc5b9a99c0c15ca
Munkhzorig <munkhzorig.ts@gmail.com>
2024-03-31 17:46:57 +1100

    My first commit

---
+++ src/AccountServices.py
@@ -0,0 +1,17 @@
+class AccountServices:
+    
+    def __init__(self, init_balance=0):
+        self.account_balance = init_balance
+       
+
+    def transaction_deposit(self,deposit_amount):
+        return_value = {}
+        return return_value      
+
+    def transaction_withdraw(self,withdraw_amount):
+        return_value = {}
+        return return_value        
+    
+    def check_account_balance(self):
+        return_value = {}
+        return return_value  
