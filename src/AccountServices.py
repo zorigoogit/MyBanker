30eeb184dd42735ff03fcbf890762e1ccebe6ea7
Munkhzorig <munkhzorig.ts@gmail.com>
2024-03-31 18:12:05 +1100

    Edited from local repository(freature-y branch)

--- src/AccountServices.py
+++ src/AccountServices.py
@@ -10,7 +10,29 @@ class AccountServices:
 
     def transaction_withdraw(self,withdraw_amount):
         return_value = {}
-        return return_value        
+        try:
+            #
+            # if withdraw_amount <= 0:
+            #     return_value["Status"] = "ERROR"
+            #     return_value["Message"] = f"The transaction could not be completed: The amount must be positive!"            
+            #     return return_value 
+
+            # if self.account_balance < withdraw_amount:
+
+            #     return_value["Status"] = "ERROR"
+            #     return_value["Message"] = f"The transaction could not be completed: Insufficient account balance!"            
+            #     return return_value 
+            
+            self.account_balance -= withdraw_amount
+
+            return_value["Status"] = "OK"
+            return_value["Message"] = f"Your withdrawal transaction has been completed successfully.\nYour current balance = {self.account_balance}"
+            return return_value    
+        
+        except Exception as e:
+            return_value["Status"] = "ERROR"
+            return_value["Message"] = f"\nThe transaction could not be completed: {e}\n"            
+            return return_value        
     
     def check_account_balance(self):
         return_value = {}
